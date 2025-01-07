5ee13108fe1922123db353fc4d2da35d7cc46aea
pablo escalante <56638479+PabloEscalante2000@users.noreply.github.com>
2025-01-06 21:33:36 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import About from "./components/About"
+import Hero from "./components/Hero"
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar/>
+        <Hero/>
+        <About/>
+    </main>
   )
 }
 
